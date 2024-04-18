4e2bd9d19d5c653b9633a89ad6a00848ddbbb0db
your_username <olumoyinjoshua19609@gmail.com>
2024-04-18 01:14:51 +0100

    commit

--- bubblebackend/settings.py
+++ bubblebackend/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get('DEBUG')
 if DEBUG == True: 
     ALLOWED_HOSTS = ["*"]
 else:
-    DEBUG = os.environ.get('DEBUG')
+ 
     # print(DEBUG)
     ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
     SECRET_KEY = os.environ.get('SECRET_KEY')
